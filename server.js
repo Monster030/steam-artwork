b46c3bd5f9500cf429544147fcd918c8e29834eb
Monster <30232602+Monster030@users.noreply.github.com>
2021-01-04 21:16:41 +0800

    feat: check steam cdns

--- server.js
+++ server.js
@@ -25,17 +25,66 @@ app.use((req, res, next) => {
 
 app.all(/\/cors\/(.*)/, function(req, res) {
   var targetURL = req.url.replace("/cors/", "");
+  const cdns = [
+    "steampowered.com",
+    "steamcommunity.com",
+    "steamgames.com",
+    "steamusercontent.com",
+    "steamcontent.com",
+    "steamstatic.com",
+    "akamaihd.net",
+    "steambroadcast.akamaized.net",
+    "steamcdn-a.akamaihd.net",
+    "steamcommunity-a.akamaihd.net",
+    "steamstore-a.akamaihd.net",
+    "steamusercontent-a.akamaihd.net",
+    "steamuserimages-a.akamaihd.net",
+    "csgo.wmsj.cn",
+    "dl.steam.ksyna.com",
+    "dota2.wmsj.cn",
+    "st.dl.bscstorage.net",
+    "st.dl.eccdnx.com",
+    "st.dl.pinyuncloud.com",
+    "steampowered.com.8686c.com",
+    "steamstatic.com.8686c.com",
+    "s.team",
+    "steam-chat.com",
+    "valvesoftware.com"
+  ];
+
   if (!targetURL) {
-    res.send(400, {
+    res.status(400).send({
       status: 400,
       msg: "bad request"
     });
     return;
   }
-  request({
-    url: targetURL,
-    method: req.method
-  }).pipe(res);
+  var _isSteamValidUrl = str => {
+    let url;
+    try {
+      url = new URL(str);
+    } catch (_) {
+      return false;
+    }
+    str;
+    var web = url.protocol === "http:" || url.protocol === "https:";
+    var cdn = cdns.find(ele => url.hostname.includes(ele));
+
+    return web && cdn;
+  };
+
+  if (_isSteamValidUrl(targetURL))
+    request({
+      url: targetURL,
+      method: req.method
+    }).pipe(res);
+  else {
+    res.status(400).send({
+      status: 400,
+      msg: "bad request"
+    });
+    return;
+  }
 });
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
